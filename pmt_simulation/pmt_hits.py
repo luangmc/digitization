c468c442bdb964968bb4a00265ce25c5490faf7e
Luan Gomes <luan.gomes@engenharia.ufjf.br>
2023-10-07 18:15:02 -0300

    Improve pmt_hits code readability
    Some modifications to standardize the code of the functions used to calculate the hits.

--- pmt_simulation/pmt_hits.py
+++ pmt_simulation/pmt_hits.py
@@ -19,72 +19,77 @@ class PhotonPropagation:
         return params
 
     def sim_pmt_hits(self, x_0, y_0, n_photons):
-            hits = {key: 0 for key in ['pmt_1', 'pmt_2', 'pmt_3', 'pmt_4']}
-            pmt_positions = self.params['pmt_positions']
-            pmt_radius_sq = self.params['pmt_radius']**2
+        hits = {key: 0 for key in ['pmt_1', 'pmt_2', 'pmt_3', 'pmt_4']}
+        pmt_positions = self.params['pmt_positions']
+        pmt_radius_sq = self.params['pmt_radius']**2
 
-            for _ in range(n_photons):
-                n = 3
-                components = [np.random.normal() for i in range(n)]
-                r = math.sqrt(sum(x*x for x in components))
-                u = [x/r for x in components]
-                if u[1] < 0:
-                    continue
+        for _ in range(n_photons):
+            n = 3
+            components = [np.random.normal() for i in range(n)]
+            r = math.sqrt(sum(x*x for x in components))
+            u = [x/r for x in components]
+            if u[1] < 0:
+                continue
 
-                t = (self.params['dist_gem_pmt'] - self.z_0) / u[2]
-                x = x_0 + t * u[0]
-                y = y_0 + t * u[1]
+            t = (self.params['dist_gem_pmt'] - self.z_0) / u[2]
+            x = x_0 + t * u[0]
+            y = y_0 + t * u[1]
 
-                for pmt_name, pmt_pos in pmt_positions.items():
-                    dx = x - pmt_pos['x']
-                    dy = y - pmt_pos['y']
-                    if dx**2 + dy**2 < pmt_radius_sq:
-                        hits[pmt_name] += 1
-                        break
+            for pmt_name, pmt_pos in pmt_positions.items():
+                dx = x - pmt_pos['x']
+                dy = y - pmt_pos['y']
+                if dx**2 + dy**2 < pmt_radius_sq:
+                    hits[pmt_name] += 1
+                    break
 
-            return hits
+        return hits
 
     def sim_pmt_hits_with_map(self, x_0, y_0, n_photons):
-        pmt_pos = self.params['pmt_positions']
+        pmt_positions = self.params['pmt_positions']
+        
         sigma_x = 130
         sigma_y = 130
+        
+        hits = {}
 
-        pmt_hits = {}
-
-        for pmt, params in pmt_pos.items():
-            mean_x = params['x']
-            mean_y = params['y']
+        for pmt_name, pmt_pos in pmt_positions.items():
+            x_pmt, y_pmt = pmt_pos['x'], pmt_pos['y']
 
             prob = (
-                0.00169 * np.exp(-0.5 * ((x_0 - mean_x) / sigma_x) ** 2) *
-                np.exp(-0.5 * ((y_0 - mean_y) / sigma_y) ** 2)
+                0.00169 * np.exp(-0.5 * ((x_0 - x_pmt) / sigma_x) ** 2) *
+                np.exp(-0.5 * ((y_0 - y_pmt) / sigma_y) ** 2)
             )
 
-            pmt_hits[pmt] = np.random.poisson(prob * n_photons)
+            hits[pmt_name] = np.random.poisson(prob * n_photons)
 
-        return pmt_hits
+        return hits
 
-    def sim_pmt_hits_with_eq(self, pmt_position, x_0, y_0, n_photons):
-        x_pmt, y_pmt, z_pmt = pmt_position['x'], pmt_position['y'], self.params['dist_gem_pmt']
-        r_pmt = self.params['pmt_radius']
-        n=3.8
-        R = np.sqrt((x_pmt - x_0) ** 2 + (y_pmt - y_0) ** 2 + (z_pmt - self.z_0) ** 2)
-        return np.random.poisson(n_photons * (r_pmt ** 2) * (z_pmt ** 2) / (4 * (R ** n)), 1)
+    def sim_pmt_hits_with_eq(self, x_0, y_0, n_photons):
+        pmt_positions = self.params['pmt_positions']
+        
+        hits = {}
+        
+        for pmt_name, pmt_pos in pmt_positions.items():
+            x_pmt, y_pmt, z_pmt = pmt_pos['x'], pmt_pos['y'], self.params['dist_gem_pmt']
+            r_pmt = self.params['pmt_radius']
+            n = 3.8
+            R = np.sqrt((x_pmt - x_0) ** 2 + (y_pmt - y_0) ** 2 + (z_pmt - self.z_0) ** 2)
+            
+            hits[pmt_name] = np.random.poisson(n_photons * (r_pmt ** 2) * (z_pmt ** 2) / (4 * (R ** n)))
+            
+        return hits
 
-    def pmt_hits(self, mode):
+    def pmt_hits(self, mode):   
         #Mode = 0: Use equation R^n
         #Mode = 1: Use photon by photon propagation
         #Mode = 2: Use map
+        
         hits = {}
-        pmts = ['pmt_1', 'pmt_2', 'pmt_3', 'pmt_4']
 
         for i, (x, y, p, t) in enumerate(zip(self.x_0, self.y_0, self.n_photons, self.arr_times)):
             voxel_name = 'voxel_{}'.format(i)
-            if (mode==0):
-                hits[voxel_name] = {key: 0 for key in pmts}
-                for pmt in pmts:
-                  pmt_position = self.params["pmt_positions"][pmt]
-                  hits[voxel_name][pmt] = int(self.sim_pmt_hits_with_eq(pmt_position, x, y, p))
+            if   (mode==0):
+                hits[voxel_name] = self.sim_pmt_hits_with_eq(x, y, p)
             elif (mode==1):
                 hits[voxel_name] = self.sim_pmt_hits(x, y, p)
             elif (mode==2):
